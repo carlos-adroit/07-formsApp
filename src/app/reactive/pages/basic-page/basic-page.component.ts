226f61b325b56e0e3b79d48a77228e9d32d760c6
Carlos Verdugo <carlos.versa99@gmail.com>
2025-02-06 22:25:35 -0700

    Adding form reactives

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './basic-page.component.css'
 })
 export class BasicPageComponent {
+
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('', [], []),
+  //   price: new FormControl(0, [], []),
+  //   inStorage: new FormControl(0),
+  // });
+
+  public myForm!: FormGroup;
+
+  constructor( private fb: FormBuilder ) {
+    this.myForm = this.fb.group({
+      name: ['', [ Validators.required, Validators.minLength(3) ] ],
+      price: [0, [ Validators.required, Validators.min(0) ] ],
+      inStorage: [0, [ Validators.required, Validators.min(0) ]],
+    })
+  }
+
+  onSave(): void {
+
+    if (this.myForm.invalid) return;
+
+    console.log(this.myForm.value)
+  }
 
 }
